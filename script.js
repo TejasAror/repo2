5d786309ca95573baea800ca866a75e1bc6d8053
Tejas Arora <tejasa755@gmail.com>
2024-04-25 22:59:09 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '16c1f50564msh65db9ce44f46ce8p10ccd9jsn872f84de95b1',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Delhi', options)
+    .then(response => response.json())
+    .then(response => response.log(response))
+    .catch(err => console.error(err));
+
+
+// try {
+// 	const response = await fetch(url, options);
+// 	const result = await response.text();
+// 	console.log(result);
+// } catch (error) {
+// 	console.error(error);
+// }
